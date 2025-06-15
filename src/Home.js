89f71d410285e37875c9f395ddc4ce76132cf37e
Vincent Adrian <vincentquitoriano38@gmail.com>
2025-06-15 23:22:01 +0800

    2nd commit

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
+import { useState  } from "react";
+import BlogList from "./BlogList";
+
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+        { title: 'My new Website', body: 'Esse cillum adipisicing id excepteur irure sint reprehenderit id consequat do cillum aliqua eu nostrud.', author: 'Vince', id: 1},
+        { title: 'My Life', body: 'Esse cillum adipisicing id excepteur irure sint reprehenderit id consequat do cillum aliqua eu nostrud.', author: 'Vince', id: 2},
+        { title: 'My Blog', body: 'Esse cillum adipisicing id excepteur irure sint reprehenderit id consequat do cillum aliqua eu nostrud.', author: 'Binsoy', id: 3}
+    ]);
+
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
+            <BlogList blogs={blogs} title="All Blogs!"/>
+            <BlogList blogs={blogs.filter((blog) => blog.author === "Vince")} title="Vince's Blogs!"/>
+            
         </div>
      );
 }
