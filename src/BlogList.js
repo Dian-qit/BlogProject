505f5b37d106c336a8df3ca43766e7acc2c8856f
Vincent Adrian <vincentquitoriano38@gmail.com>
2025-06-23 21:32:55 +0800

    Update: Using news API

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,17 +1,18 @@
-const BlogList = ({blogs, title, handleDelete}) => {
-    
-    return ( 
-        <div className="blog-list">
-            <h2>{ title }</h2>
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blogs.id}>
-                    <h2>{ blog.title }</h2>
-                    <p>Written by: { blog.author}</p>
-                    <button onClick={() => handleDelete(blog.id)}>Delete Blog</button>
-                </div>
-            ))}
+const BlogList = ({ blogs, title, handleDelete }) => {
+  return ( 
+    <div className="blog-list">
+      <h2>{title}</h2>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.url}>
+          <h2>{blog.title}</h2>
+          <p>{blog.url}</p>
+          <p>{blog.source.name}</p>
+          <p>Written by: {blog.author || "Unknown"}</p>
+          <button onClick={() => handleDelete(blog.url)}>Delete Blog</button>
         </div>
-     );
-}
- 
+      ))}
+    </div>
+  );
+};
+
 export default BlogList;
--- src/Home.js
+++ src/Home.js
@@ -1,33 +1,48 @@
 import { useState, useEffect  } from "react";
 import BlogList from "./BlogList";
 
-const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new Website', body: 'Esse cillum adipisicing id excepteur irure sint reprehenderit id consequat do cillum aliqua eu nostrud.', author: 'Vince', id: 1},
-        { title: 'My Life', body: 'Esse cillum adipisicing id excepteur irure sint reprehenderit id consequat do cillum aliqua eu nostrud.', author: 'Vince', id: 2},
-        { title: 'My Blog', body: 'Esse cillum adipisicing id excepteur irure sint reprehenderit id consequat do cillum aliqua eu nostrud.', author: 'Binsoy', id: 3}
-    ]);
+const apiKey = process.env.REACT_APP_API_KEY;
 
-    const [name, setName] = useState('mario');
+
+
+const Home = () => {
+    const [blogs, setBlogs] = useState(null);
+
+    // const [name, setName] = useState('mario');
 
     const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
+        const newBlogs = blogs.filter(blog => blog.url !== id);
         setBlogs(newBlogs);
     }
 
     useEffect(() => {
-        console.log('use effect has ran');
-        console.log(blogs);
 
-    }, [name]);
+        const url = `https://newsapi.org/v2/top-headlines?q=Weather&sortBy=publishedAt&apiKey=${apiKey}`;
+
+                
+        fetch(url)
+            .then(res => {
+                return res.json()
+            })
+            .then(data => {
+                console.log(data);
+                setBlogs(data.articles);
+            })
+    }, []);
+
+    // useEffect(() => {
+    //     console.log('use effect has ran');
+    //     console.log(blogs);
+
+    // }, [name]);
 
 
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+            {blogs && <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />}
             {/* <BlogList blogs={blogs.filter((blog) => blog.author === "Vince")} title="Vince's Blogs!"/> */}
-            <button onClick={() => setName('luigi')}>Change name</button>
-            <p>{name}</p>
+            {/* <button onClick={() => setName('luigi')}>Change name</button>
+            <p>{name}</p> */}
         </div>
      );
 }
